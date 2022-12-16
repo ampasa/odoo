788314dee88d7ca6e0309070646d67731d7c1da8
Mauricio Ruiz <mauricio.ruiz.pimentel@gmail.com>
2022-12-16 08:23:10 -0600

    [ADD] cfdi_boveda_fiscal

---
+++ cfdi_boveda_fiscal/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : 'CFDI Bóveda fiscal',
+    'license': 'GPL-3',
+    "version" : '15',
+    "author" : '...',
+    "category" : '',
+    "website" : "",
+    "description" : """
+            Este módulo ...
+    """,
+    "init_xml" : [],
+    "depends" : ['base','account'],
+    "data" : [                  
+        'security/ir.model.access.csv',
+        'views/cfdi_download_fiel_view.xml',
+        'views/cfdi_download_request_view.xml',
+        'views/cfdi_download_pack_view.xml',
+         'views/cfdi_download_data_view.xml',
+        'wizard/request_wizard_view.xml',                        
+        'reports/pack_qweb_report_view.xml',                        
+    ],
+    "demo_xml":[],
+    "test":[],
+    "installable": True,
+    "images": [],
+    "auto_install": False,
+}
