fcdaa069c3dd1747f868191f390f33969d6bba72
Mauricio Ruiz <mauricio.ruiz.pimentel@gmail.com>
2022-12-16 08:23:10 -0600

    [ADD] cfdi_boveda_fiscal

---
+++ cfdi_boveda_fiscal/models/cfdi_dowload_fiel.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+from odoo import api, fields, models, _, tools
+from odoo.exceptions import UserError
+from datetime import datetime
+
+
+class CfdiDownloadFiel(models.Model):
+    _name = "cfdi.download.fiel"
+    _description="CFDI Download FIEL"
+    _rec_name = 'rfc'
+
+    rfc = fields.Char(string="RFC", required=True)
+    cer_name = fields.Char(string="Certificado" )
+    cer_file = fields.Binary(string='Archivo (.cer)', required=True)
+    key_name = fields.Char(string="Llave" )
+    key_file = fields.Binary(string='Archivo (.key)', required=True)
+    password = fields.Char(string='Contraseña', required=True)
+    company_id = fields.Many2one(comodel_name="res.company",string="Compañia",default=lambda self: self.env.company, copy=True)
+
+    @api.model
+    def create(self,vals):
+        # Obtenemos las FIELS del usuario
+        n = self.search_count([])
+        if n > 0:        
+            raise UserError('Ya existe un registro de FIEL.')
+        res = super(CfdiDownloadFiel, self).create(vals)       
+        return res
