f1df85ea9c4962a810b2f4410609ec07ca290c23
DuvanZavaleta <duvzavaleta@gmail.com>
2023-01-10 13:04:46 -0500

    [ADD] Cambios de flujo de trabajo para campos
    Se agregan los cambios del flujo de trabajo de los campos flete, killing date y fecha de entrada en los modelos, stock picking -> operaciones detalladas, stock quant y mrp.

--- ahal_picking_logistics/models/stock_quant.py
+++ ahal_picking_logistics/models/stock_quant.py
@@ -5,12 +5,25 @@ from odoo import models, api, fields, _
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    kd_cargo = fields.Date(string="Killing date")
-    flete_id = fields.Many2one('flete.rel',string="Flete")
-    fecha_entrada = fields.Date(string="Fecha entrada")
+    kd_cargo = fields.Date(string="Killing date", related="lot_id.kd_cargo")
+    flete_id = fields.Many2one('flete.rel',string="Flete", related="lot_id.flete_id")
+    fecha_entrada = fields.Date(string="Fecha entrada", related="lot_id.fecha_entrada")
+    custom_field_value = fields.Char(string='Custom Field Value')
+
+    #def create(self, vals):
+    #    #Set the value of the custom field
+    #    vals['custom_field_value'] = vals['custom_field']
+    #    return super(StockQuant, self).create(vals)
+
+
+
+    """def create(self,vals):
+        vals = super(StockQuant, self).create(vals)
+        vals.update({'custom_field_value': vals['custom_field']})
+        return vals"""
 
     @api.model
     def _get_inventory_fields_write(self):    
         fields = super(StockQuant, self)._get_inventory_fields_write()     
-        return fields + ['kd_cargo', 'flete_id', 'fecha_entrada'] 
+        return fields + ['kd_cargo', 'flete_id', 'fecha_entrada']
 
