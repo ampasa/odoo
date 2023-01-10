9caf2f0e12294b3541bf2e1f782c998f5d776d4e
DuvanZavaleta <duvzavaleta@gmail.com>
2023-01-10 13:04:46 -0500

    [ADD] Cambios de flujo de trabajo para campos
    Se agregan los cambios del flujo de trabajo de los campos flete, killing date y fecha de entrada en los modelos, stock picking -> operaciones detalladas, stock quant y mrp.

---
+++ ahal_picking_logistics/models/stock_production_lot.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields, _
+
+class StockProductionLot(models.Model):
+    _inherit = 'stock.production.lot'
+
+    kd_cargo = fields.Date(string="Killing date")
+    flete_id = fields.Many2one('flete.rel',string="Flete")
+    fecha_entrada = fields.Date(string="Fecha entrada")
