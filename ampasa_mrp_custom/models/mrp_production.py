46e7ae2e29c72c7e7c69bd4c0272a853ec9134d8
Mauricio Ruiz <mauricio.ruiz.pimentel@gmail.com>
2023-01-11 11:55:49 -0600

    [ADD] ampasa_mrp_custom

---
+++ ampasa_mrp_custom/models/mrp_production.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    def _get_performance(self):
+        for rec in self:
+            #           
+            quantity_carcass = 0
+            #
+            for move in rec.move_raw_ids:
+                quantity_carcass += move.quantity_done if move.product_id.carcass_product else 0
+            #
+            rec.performance = rec.qty_producing / quantity_carcass if quantity_carcass > 0 else 0
+        return True
+
+    performance = fields.Float(string="Rendimiento", compute="_get_performance")
+    
