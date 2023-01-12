7f49d59da53bfe1b2104150acdd5a1243ccd0de6
DuvanZavaleta <duvzavaleta@gmail.com>
2023-01-12 15:37:53 -0500

    Pasar valores de vista forma a stock.move.lines

--- ahal_picking_logistics/models/stock_picking.py
+++ ahal_picking_logistics/models/stock_picking.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, api, fields, _
+from odoo.exceptions import ValidationError
+import time
 import json
 
 class StockPicking(models.Model):
@@ -104,6 +106,15 @@ class StockPicking(models.Model):
                     line.lot_id.fecha_entrada = line.fecha_entrada
         return res
 
+    def action_show_details(self):
+        res = super(StockMove,self).action_show_details()
+        for line in self.move_line_ids:
+            if line.picking_id:
+                line.kd_cargo = line.picking_id.kd_cargo
+                line.flete_id = line.picking_id.flete_id
+                line.kd_cargo = line.picking_id.fecha_entrada
+        return res
+
 
      
 
@@ -121,10 +132,10 @@ class StockMoveLine(models.Model):
 
     kd_cargo = fields.Date(string="Killing date")
     flete_id = fields.Many2one('flete.rel',string="Flete")
-    fecha_entrada = fields.Date(string="Fecha entrada")
+    fecha_entrada = fields.Date(string="Fecha entrada", default=fields.Date.today)
     #lot_id_value = fields.Many2one('stock.production.lot', string="Lot/SNumber")
     
-
+ 
     #FUNCTION 
     @api.onchange('lot_id')
     def get_values(self):
@@ -141,10 +152,46 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     def action_show_details(self):
-        res = super(StockMove,self).action_show_details()
-        for line in self.move_line_ids:
-            if line.lot_id:
-                line.kd_cargo = line.lot_id.kd_cargo
-                line.flete_id = line.lot_id.flete_id
-                line.kd_cargo = line.lot_id.fecha_entrada
-        return res
+        """ Returns an action that will open a form view (in a popup) allowing to work on all the
+        move lines of a particular move. This form view is used when "show operations" is not
+        checked on the picking type.
+        """
+        self.ensure_one()
+
+        # If "show suggestions" is not checked on the picking type, we have to filter out the
+        # reserved move lines. We do this by displaying `move_line_nosuggest_ids`. We use
+        # different views to display one field or another so that the webclient doesn't have to
+        # fetch both.
+        if self.picking_type_id.show_reserved:
+            view = self.env.ref('stock.view_stock_move_operations')
+        else:
+            view = self.env.ref('stock.view_stock_move_nosuggest_operations')
+
+        if self.product_id.tracking == "serial" and self.state == "assigned":
+            self.next_serial = self.env['stock.production.lot']._get_next_serial(self.company_id, self.product_id)
+
+        return {
+            'name': _('Detailed Operations'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'stock.move',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'res_id': self.id,
+            'context': dict(
+                self.env.context,
+                show_owner=self.picking_type_id.code != 'incoming',
+                show_lots_m2o=self.has_tracking != 'none' and (self.picking_type_id.use_existing_lots or self.state == 'done' or self.origin_returned_move_id.id),  # able to create lots, whatever the value of ` use_create_lots`.
+                show_lots_text=self.has_tracking != 'none' and self.picking_type_id.use_create_lots and not self.picking_type_id.use_existing_lots and self.state != 'done' and not self.origin_returned_move_id.id,
+                show_source_location=self.picking_type_id.code != 'incoming',
+                show_destination_location=self.picking_type_id.code != 'outgoing',
+                show_package=not self.location_id.usage == 'supplier',
+                show_reserved_quantity=self.state != 'done' and not self.picking_id.immediate_transfer and self.picking_type_id.code != 'incoming',
+                default_kd_cargo = self.picking_id.kd_cargo,
+                default_flete_id = self.picking_id.flete_id.id,
+            ),
+        }  
+
+
+   
