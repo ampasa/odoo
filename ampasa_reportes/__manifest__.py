7e94b4859c723dbaa5e90a2f123e6af30021a6b3
DuvanZavaleta <duvzavaleta@gmail.com>
2023-06-14 13:52:43 -0500

    [ADD] Modulo ampasa_reportes

---
+++ ampasa_reportes/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Ampasa Reportes',
+    'version': '1.4',
+    'description': ''' Formatos de reportes de empleados AMPASA
+    ''',
+    'category': 'Empleados',
+    'author': 'Devs',
+    'website': '',
+    'depends': [
+        'hr'
+    ],
+    'data': [
+        'report/report_contrato_empleado.xml',
+        'report/report_control_documentos.xml',
+        'report/report_etiquetas_empleado.xml',
+        'report/report_notificacion_empleado.xml',
+        'report/report_validacion_empleado.xml',
+    ],
+    'images': [],
+    'application': False,
+    'installable': True,
+    'price': 0.00,
+    'currency': 'USD',
+    'license': 'OPL-1',
+}
