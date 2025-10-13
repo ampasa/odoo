ca60590b1637876f0715b85098dee702dd0a3831
jeffrepo <jeffer.leo93@gmail.com>
2025-10-13 13:52:17 -0600

    Update

--- cfdi_boveda_fiscal/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import cfdi_download_request
-from . import cfdi_dowload_fiel
-from . import cfdi_dowload_pack
-from . import cfdi_download_data
--- stock_kardex_report/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2020, Jarsa Sistemas, S.A. de C.V.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lpgl.html).
-
-from . import stock_kardex_report_wizard
